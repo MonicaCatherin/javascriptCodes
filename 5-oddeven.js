0451c4ddf7f82f00208723e8902b61ef4c8ea3b9
MonicaCatherin <120569441+MonicaCatherin@users.noreply.github.com>
2022-12-21 19:04:13 +0530

    Add files via upload

---
+++ 5-oddeven.js
@@ -0,0 +1,15 @@
+
+const prompt = require('prompt-sync')();
+
+
+let i=0;
+for (; i <=15; ) {
+    if(i%2==0){
+        console.log(`${i} is even `);
+    }
+    else{
+        console.log(`${i} is odd `);
+    }
+    i++;
+  }
+
