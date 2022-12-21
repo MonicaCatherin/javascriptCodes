3aff275ce114f33bcf0c9a0af7d72afa9767fa0a
MonicaCatherin <120569441+MonicaCatherin@users.noreply.github.com>
2022-12-21 14:42:58 +0530

    Add files via upload
    display large number

---
+++ app.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+const no1 = prompt('enter no1');
+const no2 = prompt('enter no2');
+const amount=12
+if (no1<no2){
+    console.log(no2);
+}
+else{
+    console.log(no1);
+}
+
